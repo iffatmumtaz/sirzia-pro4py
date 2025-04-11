6eae26bad2bb758814caf6a60a79045ece306162
Iffat mumtaz <160539573+iffatmumtaz@users.noreply.github.com>
2025-04-11 23:43:14 +0500

    library_manage.py

--- library_manage.py
+++ library_manage.py
@@ -16,7 +16,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.markdown('<div class="main"><div class="title">📚 Library Management System</div></div>', unsafe_allow_html=True)
+st.markdown('<div class="main"></div>', unsafe_allow_html=True)
 
 
 # Initialize session state for books
